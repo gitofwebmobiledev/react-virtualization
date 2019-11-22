8a240fc9a8feb4f0dff8e0ccdf986983f1c47c4f
dev <dev@dev.com>
2019-11-22 02:30:21 -0700

    Scroll sync

--- src/App.js
+++ src/App.js
@@ -2,27 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { loremIpsum } from 'lorem-ipsum';
-import { List, AutoSizer, CellMeasurer, CellMeasurerCache } from "react-virtualized";
+import { List, AutoSizer, CellMeasurer, CellMeasurerCache, ScrollSync} from "react-virtualized";
 
 const rowCount = 1000;
 // const listHeight = 450;
-// const rowHeight = 50;
+const rowHeight = 50;
 // const rowWidth = 800;
 
 class App extends Component {
   constructor() {
     super();
     this.renderRow = this.renderRow.bind(this);
+    this.renderColumn = this.renderColumn.bind(this);
     this.list = Array(rowCount).fill().map((val, idx) => {
       return {
         id: idx, 
         name: 'John Doe',
         image: 'http://via.placeholder.com/40',
         text: loremIpsum({
-          count: 2, // 1 -> 2
+          count: 1,
           units: 'sentences',
-          sentenceLowerBound: 10, // 4 -> 10
-          sentenceUpperBound: 100, // 8 -> 100
+          sentenceLowerBound: 4,
+          sentenceUpperBound: 8
         })
       }
     });
@@ -62,6 +63,16 @@ class App extends Component {
     );
   }
 
+  renderColumn({ index, key, style }) {
+    return (
+      <div key={key} style={style} className="row">
+        <div className="content">
+          <div>{this.list[index].id}</div>
+        </div>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -69,25 +80,54 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <div className="list">
-          <AutoSizer>
-          {
-            ({ width, height }) => {
-              return (
-                <List
-                  width={width}
-                  height={height}
-                  deferredMeasurementCache={this.cache}
-                  rowHeight={this.cache.rowHeight}
-                  rowRenderer={this.renderRow}
-                  rowCount={this.list.length}
-                  overscanRowCount={3}
-                />
-              )
-            }
-          }
-          </AutoSizer>
-        </div>
+        <ScrollSync>
+          {({ onScroll, scrollTop, scrollLeft }) => (
+            <div className="list">
+              <AutoSizer disableWidth>
+              {
+                ({ height }) => {
+                  return (
+                    <div>
+                      <div 
+                        style={{
+                          position: 'absolute',
+                          top: 0,
+                          left: 0,
+                        }}>
+                          <List
+                            className="leftSide"
+                            width={50}
+                            height={height}
+                            rowHeight={rowHeight}
+                            scrollTop={scrollTop}
+                            rowRenderer={this.renderColumn}
+                            rowCount={this.list.length}
+                            overscanRowCount={3}  />
+                      </div>
+                      <div
+                        style={{
+                          position: 'absolute',
+                          top: 0,
+                          left: 50,
+                        }}>
+                          <List
+                            width={800}
+                            height={height}
+                            rowHeight={rowHeight}
+                            onScroll={onScroll}
+                            rowRenderer={this.renderRow}
+                            rowCount={this.list.length}
+                            overscanRowCount={3}  />
+                      </div>
+                    </div>
+                  )
+                }
+              }
+              </AutoSizer>
+            </div>
+          )
+        }
+        </ScrollSync>
       </div>
     );
   }
